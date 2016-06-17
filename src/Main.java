2a79713c927f438c8fe19b3bc1fd3bccb855292e
lizibing <470196902@qq.com>
2016-06-17 17:09:24 +0800

    2rh commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String str = "hello world!";
+        System.out.println(str);
     }
 }

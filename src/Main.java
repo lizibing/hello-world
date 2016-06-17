b355cfcaaf3c2c34e13af15a8447b02b9038331d
lizibing <470196902@qq.com>
2016-06-17 17:18:02 +0800

    deb commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         String str = "hello world!";
         System.out.println(str);
+        System.out.println("branch dev");
     }
 }
